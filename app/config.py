3006d13d469cb9a897a85a61584d960970eb5b46
haonan <469950698@qq.com>
2019-11-24 21:37:25 +0800

    updateenv

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
 import os
+
+
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
     ISSUES_PER_PAGE = 50
-    WHOOSHEE_MIN_STRING_LEN = 2
